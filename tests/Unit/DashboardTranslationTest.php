a8f9cb7e9835015a5056d7c583eccc350c65cdb3
Hetel666 <hanaetel189@gmail.com>
2026-07-28 22:31:13 +0400

    Add missing dashboard translations

---
+++ tests/Unit/DashboardTranslationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DashboardTranslationTest extends TestCase
+{
+    public function test_dashboard_labels_exist_in_every_supported_locale(): void
+    {
+        $keys = [
+            'period_latest_completed',
+            'dashboard_tab_overview',
+            'dashboard_tab_efficiency',
+            'dashboard_tab_geozones',
+            'dashboard_sections',
+            'data_updated_through',
+            'current_live',
+            'current_live_description',
+            'live_data',
+            'loading_tab',
+            'modal_tab_data',
+            'modal_tab_summary',
+            'modal_tab_filters',
+            'search_equipment',
+            'pagination',
+            'previous_page',
+            'next_page',
+            'tab_load_failed',
+            'tab_load_retry',
+        ];
+
+        foreach (['az', 'ru', 'en'] as $locale) {
+            $translations = require dirname(__DIR__, 2)."/lang/{$locale}/app.php";
+
+            foreach ($keys as $key) {
+                $this->assertArrayHasKey($key, $translations, "{$locale}.app.{$key} is missing");
+                $this->assertNotSame('', trim((string) $translations[$key]));
+            }
+        }
+    }
+}
